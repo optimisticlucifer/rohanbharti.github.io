a0ab2c38aa7809f8e80ee31890bd791decf198c9
optimisticlucifer <b20314@students.iitmandi.ac.in>
2024-04-06 03:00:58 +0530

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/rohanbharti.github.io/",
   plugins: [react()],
   build: {
     outDir: 'build',
